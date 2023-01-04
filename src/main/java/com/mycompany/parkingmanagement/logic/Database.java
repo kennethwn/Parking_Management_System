d2f5344a30cb6848ad6b2e7e255a7c8dbca26b99
unknown <kenthnov117@gmail.com>
2023-01-04 22:36:07 +0700

    Declare variable for connect to MySQL

--- src/main/java/com/mycompany/parkingmanagement/logic/Database.java
+++ src/main/java/com/mycompany/parkingmanagement/logic/Database.java
@@ -6,18 +6,31 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Database {
+  private String username;
+  private String password;
+  private Connection connect;
+  private Statement st;
+  private ResultSet rs;
+  
+  public void setUsername(String username) {this.username = username;}
+  public void setPassword(String password) {this.password = password;}
+  
+  public String getUsername() {return this.username;}
+  public String getPassword() {return this.password;}
+  
   public boolean userAuth(String email,String password) {
     try {
       Class.forName("com.mysql.cj.jdbc.Driver");
-      Connection connect = DriverManager.getConnection(
-              "jdbc:mysql://localhost:3306/parking_management_oop",
-              "root",
-              ""
+      connect = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/parking_management_oop",
+           "root",
+        ""
       );
-      Statement st = connect.createStatement();
+      
+      st = connect.createStatement();
       
       String sql = "select email, password_officer from officer where email='"+email+"' and password_officer='"+password+"'";
-      ResultSet rs = st.executeQuery(sql);
+      rs = st.executeQuery(sql);
      
       if (rs.next()) return true;
       else return false;
