e8792065c918fb11f0f0b93aed0128f7f50117cd
unknown <kenthnov117@gmail.com>
2023-01-04 22:34:37 +0700

    Add methods for bold and unbold JLabel

--- src/main/java/com/mycompany/parkingmanagement/StyleGuide.java
+++ src/main/java/com/mycompany/parkingmanagement/StyleGuide.java
@@ -1,15 +1,20 @@
 package com.mycompany.parkingmanagement;
 
 import java.awt.Color;
+import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-interface cta {
+interface Style {
   void changeColor(JPanel hover, Color rand);
   void changeColor(JLabel label, Color rand);
+  public void boldingFont(JLabel text);
+  public void unboldFont(JLabel text);
 }
 
-public class CtaButton implements cta {
+public class StyleGuide implements Style {
+  private Font f;
+  
   @Override
   public void changeColor(JPanel hover, Color rand) {
     hover.setBackground(rand);
@@ -17,6 +22,29 @@ public class CtaButton implements cta {
   
   @Override
   public void changeColor(JLabel label, Color rand) {
-    label.setBackground(rand);
+    label.setForeground(rand);
+  }
+  
+  public void changePanel(JPanel panel, JPanel target) {
+    // remove current panel
+    panel.removeAll();
+    panel.repaint();
+    panel.revalidate();
+    // add panel
+    panel.add(target);
+    panel.repaint();
+    panel.revalidate();
+  }
+  
+  @Override
+  public void boldingFont(JLabel text) {
+    f = text.getFont();
+    text.setFont(f.deriveFont(f.getStyle() | Font.BOLD));
+  }
+  
+  @Override
+  public void unboldFont(JLabel text) {
+    f = text.getFont();
+    text.setFont(f.deriveFont(f.getStyle() | ~Font.BOLD));
   }
 }
