4d112a9d6dff6f3b94f9df8d0bef9e0cb4da9906
unknown <kenthnov117@gmail.com>
2023-01-05 12:06:43 +0700

    Change Database Class concept

--- src/main/java/com/mycompany/parkingmanagement/logic/Database.java
+++ src/main/java/com/mycompany/parkingmanagement/logic/Database.java
@@ -2,44 +2,24 @@ package com.mycompany.parkingmanagement.logic;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class Database {
-  private String username;
-  private String password;
-  private Connection connect;
-  private Statement st;
-  private ResultSet rs;
+  private static Connection connect;
   
-  public void setUsername(String username) {this.username = username;}
-  public void setPassword(String password) {this.password = password;}
-  
-  public String getUsername() {return this.username;}
-  public String getPassword() {return this.password;}
-  
-  public boolean userAuth(String email,String password) {
-    try {
-      Class.forName("com.mysql.cj.jdbc.Driver");
-      connect = DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/parking_management_oop",
-           "root",
-        ""
-      );
-      
-      st = connect.createStatement();
-      
-      String sql = "select email, password_officer from officer where email='"+email+"' and password_officer='"+password+"'";
-      rs = st.executeQuery(sql);
-     
-      if (rs.next()) return true;
-      else return false;
-      
+  public static Connection getConnection() {
+    if(connect == null) {
+      try {
+        String url = "jdbc:mysql://localhost:3306/parking_management_oop";
+        String user = "root";
+        String pw = "";
+        
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        connect = DriverManager.getConnection(url,user,pw);
+      }
+      catch(Exception e) {
+        System.out.println(e.getMessage());
+      }
     }
-    catch (Exception e) {
-      System.out.println(e.getMessage());
-    }
-    return false;
+    return connect;
   }
-  
 }
