619f0a84dddcc90516c712beee8f603496cc8c42
unknown <kenthnov117@gmail.com>
2023-01-11 21:37:58 +0700

    Add parameter JTable to method

--- src/main/java/com/mycompany/parkingmanagement/logic/Vehicles/Dashboard.java
+++ src/main/java/com/mycompany/parkingmanagement/logic/Vehicles/Dashboard.java
@@ -1,9 +1,11 @@
 package com.mycompany.parkingmanagement.logic.Vehicles;
 
+import javax.swing.JTable;
+
 public class Dashboard extends com.mycompany.parkingmanagement.logic.Vehicle { 
 
   @Override
-  protected void displayData() {
+  protected void displayData(JTable table) {
     throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
   }
   
