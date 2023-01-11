59ffa811ba21f69941563425294f09d7231657ad
unknown <kenthnov117@gmail.com>
2023-01-11 21:35:19 +0700

    Add regular method connection db

--- src/main/java/com/mycompany/parkingmanagement/logic/Database.java
+++ src/main/java/com/mycompany/parkingmanagement/logic/Database.java
@@ -9,19 +9,34 @@ import java.sql.Statement;
 public class Database {
   private static Connection connect;
   
+  private static String url = "jdbc:mysql://localhost:3306/parkir_system";
+  private static String user = "root";
+  private static String pw = "";
+  
   public PreparedStatement preparedStatement;
   public Statement st;
   public Connection connector;
   public ResultSet rs;
   
+  // Prepared Statement
   public static Connection getConnection() {
     if(connect == null) {
       try {
-        String url = "jdbc:mysql://localhost:3306/parkir_system";
-        String user = "root";
-        String pw = "";
-        
         DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        connect = DriverManager.getConnection(url,user,pw);
+      }
+      catch(Exception e) {
+        System.out.println(e.getMessage());
+      }
+    }
+    return connect;
+  }
+  
+  // Regular Statement
+  public static Connection getConnection2() {
+    if(connect == null) {
+      try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
         connect = DriverManager.getConnection(url,user,pw);
       }
       catch(Exception e) {
