3afd3c6fc75f209c19fbd2463b6d48fba86372ac
unknown <kenthnov117@gmail.com>
2023-01-11 21:37:24 +0700

    Fixed bug to displayData()

--- src/main/java/com/mycompany/parkingmanagement/logic/Vehicles/VehicleList.java
+++ src/main/java/com/mycompany/parkingmanagement/logic/Vehicles/VehicleList.java
@@ -2,31 +2,36 @@ package com.mycompany.parkingmanagement.logic.Vehicles;
 
 import com.mycompany.parkingmanagement.MainView;
 import com.mycompany.parkingmanagement.logic.Database;
+import java.sql.SQLException;
+import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 public class VehicleList extends com.mycompany.parkingmanagement.logic.Vehicle {
   private static int count;
+  
+  public static int getAllUnemptySlot() {
+    return count;
+  }
 
   @Override
-  @SuppressWarnings("empty-statement")
-  public void displayData() {
-    try {
-      db.connector = Database.getConnection();
-      
-      String sql = "select"
-              + "kendaraan.nopol as No_Polisi,"
-              + "tipe_kendaraan as Tipe,"
-              + "merk as Merk,"
-              + "jam_masuk as Jam_Masuk"
-              + "from kendaraan_payment"
-              + "inner join kendaraan on kendaraan_payment.nopol = kendaraan.nopol"
-              + "inner join membership on kendaraan.id_membership = membership.id_membership"
-              + "inner join jenis_kendaraan on kendaraan.id_jenis_kendaraan = jenis_kendaraan.id_jenis_kendaraan"
-              + "inner join payment on kendaraan_payment.id_payment = payment.id_payment"
-              + "inner join status_payment on kendaraan_payment.id_status_payment = status_payment.id_status_payment"
-              + "where kendaraan.jam_keluar is null and staus_payment.id_status_payment = 'BL';";
-      
+  public void displayData(JTable table) {
+   try {
+      db.connector = Database.getConnection2();
       db.st = db.connector.createStatement();
+      
+      String sql = "SELECT \n" +
+                "kendaraan.nopol as No_Polisi,\n" +
+                "tipe_kendaraan as Tipe,\n" +
+                "merk as Merk,\n" +
+                "jam_masuk as Jam_Masuk\n" +
+                "FROM kendaraan_payment\n" +
+                "inner join kendaraan on kendaraan_payment.nopol = kendaraan.nopol\n" +
+                "inner join membership on kendaraan.id_membership = membership.id_membership\n" +
+                "inner join jenis_kendaraan on kendaraan.id_jenis_kendaraan = jenis_kendaraan.id_jenis_kendaraan\n" +
+                "inner join payment on kendaraan_payment.id_payment = payment.id_payment\n" +
+                "inner join status_payment on kendaraan_payment.id_status_payment = status_payment.id_status_payment\n" +
+                "where kendaraan.jam_keluar is null AND status_payment.id_status_payment = 'BL';";
+      
       db.rs = db.st.executeQuery(sql);
       
       while(db.rs.next()) {
@@ -35,17 +40,16 @@ public class VehicleList extends com.mycompany.parkingmanagement.logic.Vehicle {
         setVehicleBrand(db.rs.getString("Merk"));
         setTimeEntry(String.valueOf(db.rs.getTime("Jam_Masuk")));
         
-        String[] tbData = {license_plate,vehicle_type,vehicle_brand,time_entry};
-        DefaultTableModel tbModel = (DefaultTableModel)MainView.jTable1.getModel();
+        String tbData[] = {license_plate,vehicle_type,vehicle_brand,time_entry};
+        
+        DefaultTableModel tbModel = (DefaultTableModel)table.getModel();
         tbModel.addRow(tbData);
       }
+      
+      db.connector.close();
     }
-    catch (Exception e) {
-      throw new UnsupportedOperationException("Not connected to Database");
+    catch (SQLException e) {
+      System.out.println(e.getMessage());
     }
-  }
-  
-  public static int getAllUnemptySlot() {
-    return count;
   }
 }
