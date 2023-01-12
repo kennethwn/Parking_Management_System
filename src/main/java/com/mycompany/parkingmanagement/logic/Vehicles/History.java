e44140ac30e3d9311e1adb69fbfefabbe2447821
unknown <kenthnov117@gmail.com>
2023-01-12 21:51:54 +0700

    Create method for display data history

--- src/main/java/com/mycompany/parkingmanagement/logic/Vehicles/History.java
+++ src/main/java/com/mycompany/parkingmanagement/logic/Vehicles/History.java
@@ -1,5 +1,55 @@
 package com.mycompany.parkingmanagement.logic.Vehicles;
 
-public class History {
+import com.mycompany.parkingmanagement.logic.Database;
+import java.sql.SQLException;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
+public class History extends com.mycompany.parkingmanagement.logic.Vehicle {
+
+  @Override
+  public void displayData(JTable table) {
+    try {
+      db.connector = Database.getConnection2();
+      db.st = db.connector.createStatement();
+      
+      super.sql = "SELECT \n" +
+                "kendaraan.nopol as No_Polisi,\n" +
+                "tipe_kendaraan as Tipe,\n" +
+                "merk as Merk,\n" +
+                "jam_masuk as Jam_Masuk,\n" +
+                "jam_keluar as Jam_Keluar\n" +
+                "FROM kendaraan_payment\n" +
+                "inner join kendaraan on kendaraan_payment.nopol = kendaraan.nopol\n" +
+                "inner join membership on kendaraan.id_membership = membership.id_membership\n" +
+                "inner join jenis_kendaraan on kendaraan.id_jenis_kendaraan = jenis_kendaraan.id_jenis_kendaraan\n" +
+                "inner join payment on kendaraan_payment.id_payment = payment.id_payment\n" +
+                "inner join status_payment on kendaraan_payment.id_status_payment = status_payment.id_status_payment\n" +
+                "where jam_keluar is not null AND status_payment.id_status_payment = 'L';";
+      
+      db.rs = db.st.executeQuery(sql);
+      
+      while(db.rs.next()) {
+        setLicensePlate(db.rs.getString("No_Polisi"));
+        setVehicleType(db.rs.getString("Tipe"));
+        setVehicleBrand(db.rs.getString("Merk"));
+        setTimeEntry(String.valueOf(db.rs.getTime("Jam_Masuk")));
+        setTimeExit(String.valueOf(db.rs.getTime("Jam_Keluar")));
+        
+        String tbData[] = {
+          super.license_plate,
+          super.vehicle_type,
+          super.vehicle_brand,
+          super.time_entry,
+          super.time_exit
+        };
+        
+        DefaultTableModel tbModel = (DefaultTableModel)table.getModel();
+        tbModel.addRow(tbData);
+      }
+    }
+    catch (SQLException e) {
+      System.out.println(e.getMessage());
+    }
+  }
 }
